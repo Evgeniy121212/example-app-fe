47348f78635da7546383c08543089f747f5a74bf
Evgeniy <evolkovskiy@12devs.com>
2024-01-15 15:47:56 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [appName, setAppName] = useState('click to load app Name');
+
+  async function loadAppHandler() {
+    const url = `${process.env.REACT_APP_BACKEND_URL}/app-name`;
+    console.log(`request to`, url);
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result);
+    setAppName(result.data);
+  }
+
+  async function saveHandler() {
+    const key = prompt('enter key:');
+    const value = prompt('enter value:');
+    const url = `${process.env.REACT_APP_BACKEND_URL}/redis/save?key=${key}&value=${value}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result);
+    alert('ok');
+  }
+
+  async function getHandler() {
+    const key = prompt('enter key:');
+    const url = `${process.env.REACT_APP_BACKEND_URL}/redis/get?key=${key}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    console.log(result);
+    alert(`got: "${result.data}"`);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +48,12 @@ function App() {
         >
           Learn React
         </a>
+
+        <button className='my-button' onClick={loadAppHandler}>{appName}</button>
+        <button className='my-button' onClick={saveHandler}>Save Value</button>
+        <button className='my-button' onClick={getHandler}>Get Value</button>
+
+        (Note: saving is case sensitive)
       </header>
     </div>
   );
